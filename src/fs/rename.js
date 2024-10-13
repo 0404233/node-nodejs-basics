80c4983f782661e2b17caedfe4e1978a90f65b78
0404233 <andrey0404233@gmail.com>
2024-10-13 20:07:40 +0300

    fix: path to file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,8 +1,13 @@
 import { promises as fs, access } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const rename = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const file = join(__dirname, 'files');
     try {
-        await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+        await fs.rename(`${file}\\wrongFilename.txt`, `${file}\\properFilename.md`);
         console.log('File renamed successfully');
 
     } catch (err) {
