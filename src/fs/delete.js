2e0b1f0b2c445d5b0c62ccea1e535512fabc982a
0404233 <andrey0404233@gmail.com>
2024-10-13 20:00:34 +0300

    fix: path to file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,8 +1,12 @@
 import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const remove = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
     try {
-        await fs.unlink('./files/fileToRemove.txt');
+        await fs.unlink(join(__dirname, 'files', 'fileToRemove.txt'));
         console.log('File deleted');
     } catch (err) {
         console.error('FS operation failed');
