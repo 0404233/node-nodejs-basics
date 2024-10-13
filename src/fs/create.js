36170ab50a16e0635e6b7e13ef0073b73fa2e378
0404233 <andrey0404233@gmail.com>
2024-10-13 19:54:15 +0300

    fix: path to file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,15 @@
 import { writeFile, access } from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const create = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
     try {
-        await access('files/fresh.txt');
+        await access(`${__dirname}//files//fresh.txt`);
         console.log('FS operation failed');
     } catch (err) {
-        await writeFile('files/fresh.txt', 'I am fresh and young');
+        await writeFile(`${__dirname}//files//fresh.txt`, 'I am fresh and young');
         console.log('File created');
     }
 };
