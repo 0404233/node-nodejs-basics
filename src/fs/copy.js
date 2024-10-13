94b70300686f058f99e3ab498ae7820c73adc20b
0404233 <andrey0404233@gmail.com>
2024-10-13 19:50:39 +0300

    fix: fix path to folders

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,13 +1,18 @@
 import { access, cp } from 'fs/promises';
 import { constants } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const copy = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
     try {
         try {
             await access('./files_copy', constants.F_OK);
             console.log('FS operation failed');
           } catch (err) {
-            await cp('./files', './files_copy', { recursive: true });
+            await cp(`${__dirname}\\files`, `${__dirname}\\files_copy`, { recursive: true });
             console.log('Folder copied');
           }
       } catch (err) {
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,8 +1,14 @@
 import { promises as fs } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const list = async () => {
+    
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
     try {
-        const files = await fs.readdir('./files');
+        const files = await fs.readdir(__dirname);
 
         files.forEach(file => {
             console.log(file);
