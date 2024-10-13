6a71983b7573943ed3b12817191e6a4356dd5291
0404233 <andrey0404233@gmail.com>
2024-10-13 20:04:21 +0300

    fix: path to file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,8 +1,14 @@
 import { promises as fs } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const read = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const file = join(__dirname, 'files', 'fileToRead.txt');
+
     try {
-        const content = await fs.readFile('./files/fileToRead.txt', 'utf8');
+        const content = await fs.readFile(file, 'utf8');
         console.log(content);
     } catch (err) {
         console.error('FS operation failed');
