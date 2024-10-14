8a061c4d2d5bc3e4b7054001e257d31b163a83c8
0404233 <andrey0404233@gmail.com>
2024-10-14 17:27:25 +0300

    feat: implement function that parses command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = process.argv.slice(2);
+    const argsObj = {};
+
+    for(let i = 0; i < argsArr.length; i += 2) {
+        argsObj[argsArr[i]] = argsArr[i + 1];
+    }
+
+    let result = [];
+
+    Object.keys(argsObj).forEach(key => {
+        result.push(`${key} is ${argsObj[key]}`);
+    });
+
+    console.log(result.join("\n"))
 };
 
 parseArgs();
