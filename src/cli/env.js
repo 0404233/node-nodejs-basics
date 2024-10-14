13e96c6073741376b682873a80b5cb59715594f3
0404233 <andrey0404233@gmail.com>
2024-10-14 16:58:52 +0300

    feat: implement function that parses environment variables with prefix

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    let envArr = [];
+
+    Object.keys(process.env).forEach((envKey) => {
+        if(envKey.startsWith("RSS_")) {
+            envArr.push(`${envKey}=${process.env[envKey]};`);
+        }
+    });
+
+    console.log(envArr.join("\n"))
 };
 
 parseEnv();
